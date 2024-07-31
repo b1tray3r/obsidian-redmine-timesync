8654e07038598ac9c2e1c676cbc08630dd3a7196
aborgardt <alexander.borgardt@gmail.com>
2024-07-31 23:22:48 +0200

    implement plugin to send requests to a service

--- main.ts
+++ main.ts
@@ -1,81 +1,35 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, PluginSettingTab, requestUrl, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TimeSyncSettings {
+	TimeSyncServiceURL: string;
+	TimeSyncServiceToken: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TimeSyncSettings = {
+	TimeSyncServiceURL: '',
+	TimeSyncServiceToken: '',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TimeSyncPlugin extends Plugin {
+	settings: TimeSyncSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'sync-redmine-timeentries-from-selection',
+			name: 'Sync time entries',
+			callback: () => {
+				new TimeSyncModal(this.app, this.settings).open()
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new TimeSyncSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,44 +45,117 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TimeEntry {
+	date: String
+	duration: Number
+	tags: String[]
+	note: String
+}
+
+class TimeSyncModal extends Modal {
+	settings: TimeSyncSettings;
+
+	constructor(app: App, settings: TimeSyncSettings) {
 		super(app);
+		this.settings = settings
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async onOpen() {
+		const { contentEl } = this;
+
+		const file = this.app.workspace.getActiveFile()
+		if (file) {
+			let result: TimeEntry[] = []
+			const regex = /^\|\W\W*\|.*\|/gm;
+
+			let content = await this.app.vault.read(file)
+			let m;
+			while ((m = regex.exec(content)) !== null) {
+				// This is necessary to avoid infinite loops with zero-width matches
+				if (m.index === regex.lastIndex) {
+					regex.lastIndex++;
+				}
+
+				// The result can be accessed through the `m`-variable.
+				m.forEach((match, _) => {
+					let data = match.split('|')
+					let entry = new TimeEntry()
+
+					entry.date = file.name.replace('.md', '')
+					entry.duration = Number(data[2].trim())
+					entry.tags = data[3].trim().split(" ")
+					entry.note = data[4].trim()
+
+					result.push(entry)
+				});
+			}
+
+			console.log(result)
+			console.log("Starting request with: ")
+			let requestURLParam = {
+				url: this.settings.TimeSyncServiceURL,
+				method: 'POST',
+				body: JSON.stringify(result),
+				headers: {
+					'Authorization': `Basic ${this.settings.TimeSyncServiceToken}`,
+				}
+			};
+			console.log(requestURLParam)
+
+			const response = await requestUrl(requestURLParam);
+
+			console.log(response)
+
+			content = ""
+			result.forEach(element => {
+				content += `${JSON.stringify(element)}\n`
+			});
+			contentEl.append(content);
+		} else {
+			contentEl.setText("empty")
+		}
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class TimeSyncSettingTab extends PluginSettingTab {
+	plugin: TimeSyncPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TimeSyncPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('TimeSyncServiceURL')
+			.setDesc('The URL instance.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the URL')
+				.setValue(this.plugin.settings.TimeSyncServiceURL)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.TimeSyncServiceURL = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('TimeSyncServiceToken')
+			.setDesc('The token to use')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.TimeSyncServiceToken)
+				.onChange(async (value) => {
+					this.plugin.settings.TimeSyncServiceToken = value;
+					await this.plugin.saveSettings();
+				}));
+
 	}
 }
